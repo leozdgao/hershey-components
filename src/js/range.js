9203c63ed739ca3fc54096b3c581ed23f1ab6924
leozdgao <gzdleo0816@gmail.com>
2015-03-26 23:45:34 +0800

    add progress

---
+++ src/js/range.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function ($) {
+  // var animate
+  
+  $.fn.extend({
+    progress: function(percentage) {
+      percentage = percentage >  100 ? 100 : percentage;
+      this.each(function() {
+        var $target = $(this);
+        var $backdrop = $target.children('.backdrop');
+        // init state
+        if($backdrop.length <= 0) {
+          $backdrop = $('<div class="backdrop"></div>');
+          $backdrop.appendTo($target);
+        }
+
+        var width = $target.width() *percentage / 100;
+        $backdrop.width(width);
+      });
+      return this;
+    }
+  });
+
+})(jQuery);

738a2ab0c701839822059aa3143cb734840ef3d0
leozdgao <gzdleo0816@gmail.com>
2015-05-05 00:05:58 +0800

    improve taginput

--- dist/hershey.js
+++ dist/hershey.js
@@ -328,6 +328,7 @@
 	     *     <input type="text">
 	     *   </div>
 	     */
+	//    var self = this;
 	    return React.DOM.div({ className: 'tag-input', onClick: this._click },
 	             this.state.tags.map(function (tag) {
 	               return React.DOM.span({ className: 'tag' }, tag); 
@@ -335,23 +336,43 @@
 	             React.DOM.input({ type: "text", ref: 'input', onKeyDown: this._keyDown })
 	      	   );
 	  },
+	  addTag: function (val) {
+	    var tags = this.state.tags;
+	    if(val && tags.indexOf(val) < 0) tags.push(val);
+	    
+	    this.setState({ tags: tags });
+	  },
+	  removeTag: function (i) { console.log('remove');
+	    var tags = this.state.tags;
+	    if(typeof i == 'undefined') i = tags.length - 1;
+	    tags.splice(i, 1);
+	    
+	    this.setState({ tags: tags });
+	  },
 	  _click: function () {
 	    var input = React.findDOMNode(this.refs.input);
 	    input.focus();
 	  },
 	  _keyDown: function (e) {
-	    // ','
-	    if(e.keyCode === 188) {
-	      e.preventDefault();
-	      
-	      var input = React.findDOMNode(this.refs.input);
-	      var val = input.value.trim();
-	      var tags = this.state.tags;
-	      
-	      if(val && tags.indexOf(val) < 0) tags.push(val);
-	      input.value = '';
-	      
-	      this.setState({ tags: tags });
+	    var input = React.findDOMNode(this.refs.input);
+	    
+	    switch(e.keyCode) {
+	      case 188: { // add tag if ','
+	        e.preventDefault();  // prevent the input of ','
+	          
+	        var val = input.value.trim();
+	        input.value = "";
+	//        setTimeout(function () { input.value = ''; }); 
+	        this.addTag(val);
+	        break;
+	      };
+	      case 8: { // remove tag if 'del'
+	        if(input.value == '') this.removeTag(); 
+	        break;
+	      };
+	      default: {
+	        // dynamic adjust input width
+	      };
 	    }
 	  }
 	});
